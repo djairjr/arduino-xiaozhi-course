2aa85499942141f3fd9ed369c92f90a41272f297
winwill2012 <qifuguang@gmail.com>
2025-07-10 13:22:31 +0800

    增加LVGL样式讲解

--- L29_lvgl_core_concept/my_ui.c
+++ L29_lvgl_core_concept/my_ui.c
@@ -7,14 +7,5 @@
 
 void show_ui()
 {
-    // 创建面板并设置流式布局
-    lv_obj_t *panel = lv_obj_create(lv_scr_act());
-    lv_obj_set_size(panel, 480, 320);
-    lv_obj_set_layout(panel, LV_LAYOUT_FLEX);  // 左对齐流式布局
-
-    // 在面板中创建多个按钮，自动排列
-    for(int i = 0; i < 20; i++) {
-        lv_obj_t *btn = lv_btn_create(panel);
-        lv_obj_set_size(btn, 60, 30);
-    }
+    lv_img_dsc_t img_dsc;
 }
