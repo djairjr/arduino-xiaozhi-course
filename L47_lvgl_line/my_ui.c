cf0adf1de00f9bbff2b471f3b702f10879470bd5
winwill2012 <qifuguang@gmail.com>
2025-08-07 14:15:29 +0800

    更新LVGL线段使用案例

--- L47_lvgl_line/my_ui.c
+++ L47_lvgl_line/my_ui.c
@@ -3,21 +3,38 @@
 
 void show_ui()
 {
-    lv_obj_t* spinbox = lv_spinbox_create(lv_scr_act());
-    lv_obj_set_size(spinbox, 80, 40);
-    lv_obj_center(spinbox);
+    // 1. 定义线段的点坐标（可以是多个点，形成折线）
+    // 注意：这里的这些点必须设置为static，防止函数调用结束后，内存被回收
+    static lv_point_precise_t points[] = {
+        {0, 0}, // 起点 (x, y)
+        {200, 0}, // 第2个点
+        {200, 200}, // 第3个点
+        {0, 200}, // 第4个点
+        {0, 0}, // 第5个点
+        {200, 200}, // 第6个点
+        {0, 200}, // 第7个点
+        {200, 0}, // 第8个点
+        {100, 0}, // 第9个点
+        {100, 200}, // 第10个点
+        {0, 200}, // 第11个点
+        {0, 100}, // 第12个点
+        {200, 100}, // 第13个点
+    };
+    // 2. 创建线段对象
+    lv_obj_t* line = lv_line_create(lv_scr_act()); // 在当前屏幕创建
 
-    // 设置文字居中显示
-    lv_obj_set_style_text_align(spinbox, LV_TEXT_ALIGN_CENTER, LV_PART_MAIN);
+    // 3. 设置线段的点集
+    lv_line_set_points(line, points, sizeof(points) / sizeof(lv_point_precise_t));
 
-    // 设置数字总位数为3，其中整数部分为2位，有一位小数
-    lv_spinbox_set_digit_format(spinbox, 3, 2);
-    // 设置数据范围0~100，有一位小数，所以其实数值范围是[0, 10.0]
-    lv_spinbox_set_range(spinbox, 0, 100);
-    // 设置增长步长为1，实际是0.1
-    lv_spinbox_set_step(spinbox, 2);
-    // 设置当前值为50，实际是5.0
-    lv_spinbox_set_value(spinbox, 50);
+    lv_obj_set_style_line_color(line, lv_color_hex(0xFF0000), LV_PART_MAIN); // 线段颜色设置为红色
+    lv_obj_set_style_line_width(line, 2, LV_PART_MAIN); // 线宽2像素
+    lv_obj_set_style_line_rounded(line, true, LV_PART_MAIN); // 线段拐角圆角
 
-    lv_obj_set_style_bg_color(spinbox, lv_color_hex(0x33cccc), LV_PART_MAIN);
+    // 设置虚线效果
+    // 注意： 虚线只能是水平或者垂直方向
+    lv_obj_set_style_line_dash_width(line, 5, LV_PART_MAIN); // 设置虚线每一段长度
+    lv_obj_set_style_line_dash_gap(line, 2, LV_PART_MAIN); // 设置虚线相邻两端间隔
+
+    // 将整个线段对象居中，这会导致整体进行相对位置的偏移
+    lv_obj_center(line);
 }
