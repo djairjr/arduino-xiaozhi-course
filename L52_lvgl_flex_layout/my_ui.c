d2523c071b09789938d882305b35c5005217ffd8
winwill2012 <qifuguang@gmail.com>
2025-08-17 17:07:57 +0800

    弹性布局讲解

--- L52_lvgl_flex_layout/my_ui.c
+++ L52_lvgl_flex_layout/my_ui.c
@@ -3,21 +3,33 @@
 
 void show_ui()
 {
-    lv_obj_t* menu = lv_menu_create(lv_scr_act());
-    lv_obj_set_size(menu, lv_pct(100), lv_pct(100));
-    lv_obj_center(menu);
+    // 创建一个容器
+    lv_obj_t* container = lv_obj_create(lv_scr_act());
+    // 设置容器大小为240x240
+    lv_obj_set_size(container, 240, 240);
+    // 设置容器背景颜色为浅灰色
+    lv_obj_set_style_bg_color(container, lv_color_hex(0xEEEEEE), 0);
+    // 移除容器默认的内部填充样式
+    lv_obj_set_style_pad_all(container, 0, 0);
+    // 容器居中显示
+    lv_obj_center(container);
 
-    lv_menu_set_mode_header(menu, LV_MENU_HEADER_TOP_UNFIXED);
-    lv_menu_set_mode_root_back_button(menu, LV_MENU_ROOT_BACK_BUTTON_ENABLED);
+    // 设置容器的布局方式为弹性布局
+    lv_obj_set_layout(container, LV_LAYOUT_FLEX);
+    // 设置弹性布局的主轴方向为水平方向（行优先），并且空间不够时自动换行
+    lv_obj_set_flex_flow(container, LV_FLEX_FLOW_ROW_WRAP);
+    // 依次设置子元素在主轴、交叉轴、整体方向的对齐方式
+    lv_obj_set_flex_align(container, LV_FLEX_ALIGN_CENTER, LV_FLEX_ALIGN_END, LV_FLEX_ALIGN_END);
 
-    // 创建菜单侧边栏
-    lv_obj_t* sider_page = lv_menu_page_create(menu, "Settings");
-    lv_obj_t* section = lv_menu_section_create(sider_page);
-    lv_obj_t* cont = lv_menu_cont_create(section);
-    lv_obj_t* system_settings_label = lv_label_create(cont);
-    lv_label_set_text(system_settings_label, LV_SYMBOL_SETTINGS" System");
-    lv_obj_center(system_settings_label);
-
-    lv_menu_separator_create(sider_page);
-    lv_menu_set_sidebar_page(menu, sider_page);
+    // 循环创建9个按钮
+    for (int i = 0; i < 9; i++)
+    {
+        lv_obj_t* btn = lv_button_create(container);
+        lv_obj_set_style_margin_all(btn, 0, 0);
+        // 为按钮设置不同的高度，以便观察交叉轴方向的对齐效果
+        lv_obj_set_size(btn, 70, i % 2 ? 40 : 50);
+        lv_obj_t* label = lv_label_create(btn);
+        lv_label_set_text_fmt(label, "%d", i + 1);
+        lv_obj_center(label);
+    }
 }
