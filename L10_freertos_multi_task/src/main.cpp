7a5c357d918126ef4a047897558c5a086fdb066c
Djair Guilherme <djair.jr@gmail.com>
2025-09-23 10:21:06 -0300

    Update main.cpp - Translated comments to English

--- L10_freertos_multi_task/src/main.cpp
+++ L10_freertos_multi_task/src/main.cpp
@@ -10,11 +10,11 @@ void printMessage(void* ptr)
     Serial.println(static_cast<char*>(ptr));
     while (true)
     {
-        Serial.printf("当前任务参数: %s, 运行CPU: %d\n", static_cast<char*>(ptr), xPortGetCoreID());
+        Serial.printf("Current task parameter: %s, Running CPU: %d\n", static_cast<char*>(ptr), xPortGetCoreID());
         vTaskDelay(pdMS_TO_TICKS(1000));
     }
-    // 这是第一个永久运行的任务，所以该行代码不会被运行，如果是会结束的任务，
-    // 则需要在任务的最后，删除当前任务，参数为NULL表示删除自己
+    // This is the first permanently running task, so this line of code will not be executed. If it were a task that would end,
+    // it would need to delete the current task at the end, with NULL as the parameter indicating to delete itself.
     vTaskDelete(nullptr);
 }
 
@@ -22,41 +22,41 @@ void setup()
 {
     Serial.begin(9600);
     vTaskDelay(pdMS_TO_TICKS(3000));
-    xTaskCreate(printMessage, // 任务函数指针
-                "printMessage1", // 任务名称
-                2048, // 任务堆栈大小，简单的任务堆栈可以设置小一些，复杂的任务需要设置大一点得到堆栈，不然容易堆栈溢出
-                const_cast<char*>("这是第1个任务参数"), // 需要传递什么参数到任务处理函数中
-                1, // 任务优先级，可选范围为0-24，数字越大，优先级越高
-                &printMessageTask1); // 任务句柄，如果后续对该任务有控制需求，可以设置该参数，TaskHandle_t类型
+    xTaskCreate(printMessage, // Task function pointer
+                "printMessage1", // Task name
+                2048, // Task stack size, simple tasks can have a smaller stack, complex tasks need a larger stack to avoid stack overflow
+                const_cast<char*>("This is the first task parameter"), // What parameter to pass to the task handler function
+                1, // Task priority, optional range 0-24, higher number means higher priority
+                &printMessageTask1); // Task handle, if there is a need to control this task later, set this parameter, TaskHandle_t type
 
-    xTaskCreate(printMessage, // 任务函数指针
-                "printMessage2", // 任务名称
-                4096, // 任务堆栈大小，简单的任务堆栈可以设置小一些，复杂的任务需要设置大一点得到堆栈，不然容易堆栈溢出
-                const_cast<char*>("这是第2个任务参数"), // 需要传递什么参数到任务处理函数中
-                2, // 任务优先级，可选范围为0-24，数字越大，优先级越高
-                &printMessageTask2); // 任务句柄，如果后续对该任务有控制需求，可以设置该参数，TaskHandle_t类型
+    xTaskCreate(printMessage, // Task function pointer
+                "printMessage2", // Task name
+                4096, // Task stack size, simple tasks can have a smaller stack, complex tasks need a larger stack to avoid stack overflow
+                const_cast<char*>("This is the second task parameter"), // What parameter to pass to the task handler function
+                2, // Task priority, optional range 0-24, higher number means higher priority
+                &printMessageTask2); // Task handle, if there is a need to control this task later, set this parameter, TaskHandle_t type
 
     vTaskDelay(pdMS_TO_TICKS(5000));
-    // 5s以后，删除以上两个任务
+    // After 5 seconds, delete the above two tasks
     vTaskDelete(printMessageTask1);
     vTaskDelete(printMessageTask2);
 
     xTaskCreatePinnedToCore(printMessage,
                             "printMessage3",
                             2048,
-                            const_cast<char*>("第3个任务参数"),
+                            const_cast<char*>("Third task parameter"),
                             1,
                             &printMessageTask3,
                             0);
     xTaskCreatePinnedToCore(printMessage,
                             "printMessage4",
                             2048,
-                            const_cast<char*>("第4个任务参数"),
+                            const_cast<char*>("Fourth task parameter"),
                             2,
                             &printMessageTask4,
                             1);
     vTaskDelay(pdMS_TO_TICKS(5000));
-    // 5s以后，删除以上两个任务
+    // After 5 seconds, delete the above two tasks
     vTaskDelete(printMessageTask3);
     vTaskDelete(printMessageTask4);
 }
